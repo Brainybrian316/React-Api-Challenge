c94d097385ec91c7abb25c3b8502898864cd29c8
Brainybrian316 <brainybrian316@gmail.com>
2022-09-19 16:46:24 -0400

    added a button to display the users when clicked

--- api-challenge/src/components/home.js
+++ api-challenge/src/components/home.js
@@ -4,9 +4,15 @@ const Home = () => {
 		.then((response) => response.json()) // convert the response to json
 		.then((data) => console.log(data)); // log the data to the console
 
+	function handleClick() {
+		console.log('The button was clicked.');
+	}
+
 	return (
 		<div>
 			<h1>Api request</h1>
+			<p>To display the users click the button</p>
+			<button onClick={handleClick}>Click me</button>
 		</div>
 	);
 };
