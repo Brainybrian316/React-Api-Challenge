e5413f40f51b6c18ec2331c21836edcff085bb78
Brainybrian316 <brainybrian316@gmail.com>
2022-09-19 16:48:23 -0400

    button click displays data in console

--- api-challenge/src/components/home.js
+++ api-challenge/src/components/home.js
@@ -1,12 +1,15 @@
 const Home = () => {
 	const url = 'https://jsonplaceholder.typicode.com/users';
-	fetch(url) // fetch the data from the url
-		.then((response) => response.json()) // convert the response to json
-		.then((data) => console.log(data)); // log the data to the console
 
-	function handleClick() {
-		console.log('The button was clicked.');
-	}
+	const getData = () => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
+
+	const handleClick = () => {
+		getData();
+	};
 
 	return (
 		<div>
