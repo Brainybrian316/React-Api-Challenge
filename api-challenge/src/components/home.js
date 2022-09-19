02874d493418f7b3d7d8690dccc648db1c2c4391
Brainybrian316 <brainybrian316@gmail.com>
2022-09-19 17:01:12 -0400

    set state

--- api-challenge/src/components/home.js
+++ api-challenge/src/components/home.js
@@ -1,22 +1,36 @@
+import { useState } from 'react';
+
 const Home = () => {
+	const [data, setData] = useState(null);
 	const url = 'https://jsonplaceholder.typicode.com/users';
 
 	const getData = () => {
 		fetch(url)
 			.then((response) => response.json())
-			.then((data) => console.log(data));
+			.then((data) => setData(data))
+			.catch((error) => console.log(error));
 	};
 
+	// this is a higher order function because a function is being returned inside of a function
 	const handleClick = () => {
 		getData();
 	};
 
 	return (
-		<div>
+		<>
 			<h1>Api request</h1>
 			<p>To display the users click the button</p>
-			<button onClick={handleClick}>Click me</button>
-		</div>
+			<button onClick={handleClick}>Display Data</button>
+			{data &&
+				data.map((user) => (
+					<div key={user.id}>
+						<ul>
+							<li>{user.name}</li>
+							<li>{user.username}</li>
+						</ul>
+					</div>
+				))}
+		</>
 	);
 };
 
